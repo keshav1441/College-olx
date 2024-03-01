bbba60f792545ac028ac541edc8990861bfa9cf6
Sankalp0030 <sankalp3011@gmail.com>
2024-03-01 23:42:37 +0530

    first commit

---
+++ products/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+import uuid
+
+
+class BaseModel(models.Model):
+    uid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+
+class Category(BaseModel):
+    category_name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, null=True, blank=True)
+    category_image = models.ImageField(upload_to="categories")
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.category_name)
+        super(Category, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.category_name
+
+
+class Product(BaseModel):
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name="products")
+    product_name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+    price = models.IntegerField()
+    product_description = models.TextField()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.product_name)
+        super(Product, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.product_name
+
+
+class ProductImage(BaseModel):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_images")
+    image = models.ImageField(upload_to="product")
---
+++ users/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User  # Django's built-in User model
+from django.contrib import messages  # For displaying messages like errors
+from django.contrib.auth import authenticate, login
+from django.db import IntegrityError
+from .models import UserProfile
+
+
+def index(request):
+    if not request.user.is_authenticated:  
+        return HttpResponseRedirect(reverse("login"))
+    return render(request, "your_template_name.html")
+    
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("products:index"))
+        else:
+            messages.error(request, "Invalid username and/or password.")
+    return render(request, "users/login.html")
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST.get("email")
+        password = request.POST["password"]
+        confirmation = request.POST.get("confirmation")
+        college = request.POST.get("college")  # Capture college information
+        if password == confirmation:
+            try:
+                user = User.objects.create_user(username, email, password)
+                user.save()
+                user_profile = UserProfile.objects.get(user=user)
+                user_profile.college = college
+                user_profile.save()
+                login(request, user)
+                return HttpResponseRedirect(reverse("login"))
+            except IntegrityError:
+                messages.error(request, "Username already taken.")
+        else:
+            messages.error(request, "Passwords must match.")
+    return render(request, "users/register.html")
+
+
+
+    
